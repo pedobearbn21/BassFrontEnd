03d921ad435941372ab5ea5f5f3cd50bf20f3163
pedobearbn21 <pedobearearn@gmail.com>
2020-03-05 22:18:54 +0700

    [Edited] Template

--- bass/src/App.vue
+++ bass/src/App.vue
@@ -1,77 +1,102 @@
 <template>
-  <div id="app">
-    <v-app>
-    <v-app-bar
+
+    <v-app id="sandbox">
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      :clipped="primaryDrawer.clipped"
       app
-      color="primary"
+      overflow
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-list-item class="px-2 pt-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
 
-      <v-spacer></v-spacer>
+        <v-list-item-title>John Leider</v-list-item-title>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-content>
-      <v-container>
-        <router-link to="/">Home</router-link> |
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+
+
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="primaryDrawer.clipped"
+      app
+      dark
+    >
+      <v-app-bar-nav-icon
+        @click.stop="primaryDrawer.model = !primaryDrawer.model"
+      />
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+      <router-link to="/">Home</router-link> |
         <router-link to="/myblog">Myblog</router-link> |
         <router-link to="/about">About</router-link>
+    </v-app-bar>
+
+    <v-content>
+      <v-container >
         <router-view />
       </v-container>
     </v-content>
-    </v-app>
-    <!-- <div id="nav">
-      
-    </div> -->
-  </div>
-  
+
+    <v-footer
+      :inset="footer.inset"
+      app
+    >
+      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+    <!-- <v-content>
+      <v-container>
+        
+      </v-container>
+    </v-content> -->
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      primaryDrawer: {
+        model: null,
+        clipped: true,
+      },
+      footer: {
+        inset: false,
+      },
+      items: [
+          { title: 'Home', icon: 'mdi-home-city' },
+          { title: 'My Account', icon: 'mdi-account' },
+          { title: 'Users', icon: 'mdi-account-group-outline' },
+        ],
+        mini: true,
     }
-  }
+  },
 }
+</script>
+
+<style lang="scss">
+
 </style>
